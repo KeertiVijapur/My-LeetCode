c1d1162a4cb076cb049f7f929cf00e9631ce49a1
Keerti <keertivijapur.13@gmail.com>
2025-07-20 06:12:42 +0530

    Update 2. Add Two Numbers.java

--- MEDIUM/2. Add Two Numbers.java
+++ MEDIUM/2. Add Two Numbers.java
@@ -1,14 +1,13 @@
 /**
  * Problem: https://leetcode.com/problems/add-two-numbers/
- *
+ 
  * Definition for singly-linked list:
  * public class ListNode {
  *     int val;
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; } }
  */
 
 class Solution {
